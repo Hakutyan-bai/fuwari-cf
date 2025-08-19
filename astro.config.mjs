806f666b58ee732485fbf71b0c5c6be3aaab999b
NiceMiku <lzj159035@foxmail.com>
2025-08-20 00:19:38 +0800

    更新

--- astro.config.mjs
+++ astro.config.mjs
@@ -29,7 +29,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://sakura.ink/",
+	site: "https://blog.sakura.ink/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
