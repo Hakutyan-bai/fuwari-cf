8acc9c00074c2e9d5668cf0799d0e127d8b3cd31
Haku <lzj159035@foxmail.com>
2026-01-06 00:55:44 +0800

    pwaqq

--- astro.config.mjs
+++ astro.config.mjs
@@ -137,23 +137,23 @@ export default defineConfig({
 				orientation: "portrait-primary",
 				icons: [
 					{
-						src: "pwa-64x64.png",
+						src: "https://q2.qlogo.cn/headimg_dl?dst_uin=2731443459&spec=5",
 						sizes: "64x64",
 						type: "image/png",
 					},
 					{
-						src: "pwa-192x192.png",
+						src: "https://q2.qlogo.cn/headimg_dl?dst_uin=2731443459&spec=5",
 						sizes: "192x192",
 						type: "image/png",
 					},
 					{
-						src: "pwa-512x512.png",
+						src: "https://q2.qlogo.cn/headimg_dl?dst_uin=2731443459&spec=5",
 						sizes: "512x512",
 						type: "image/png",
 						purpose: "any",
 					},
 					{
-						src: "maskable-icon-512x512.png",
+						src: "https://q2.qlogo.cn/headimg_dl?dst_uin=2731443459&spec=5",
 						sizes: "512x512",
 						type: "image/png",
 						purpose: "maskable",
